c37ff95d93761a6fa1d50a90cfbc86eb70ac2701
pablofleonhart <pablo.fl11@gmail.com>
2017-10-02 16:31:20 -0300

    Implementada verificacao sat

--- gsat.py
+++ gsat.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import *
 import sys
 import time
 import resource
@@ -42,8 +42,9 @@ def main():
 	if len( clauses ) < m:
 		clauses.append( clause )            		
 
-	maxFlips = 3*n 	# number of search restarts
+	maxFlips = 3*n 	# number of search's restarts
 	maxTries = 100	# amount of time to spend looking for an assignment
+	seed = 1001
 
 	print m, n
 	print clauses, len( clauses )
@@ -53,23 +54,63 @@ def main():
 
 	for i in xrange( maxTries ):
 		# T = generates randomly truth assignment
+		t = generateAttempt( n+1, seed )
 		for j in xrange( maxFlips ):
+			res = satisfies( t, clauses )
+			if res[0]:
+				print 'OK'
 			# if T is satisfied, return T
 			# p ?
 			# T ??
 			p = 0
 
-	print generateAttempt( n )
+	v = generateAttempt( n+1, seed )
+	#v = random.randrange( 0, 1, n+1 )
+	print v
+	print satisfies( v, clauses )
 
-def generateAttempt( n ):
+def generateAttempt( n, s ):
+	seed( s )
 	result = []
 	for i in xrange( n ):
 		result.append( randint( 0, 1 ) )
 
 	return result
 
+def invValue( v ):
+	if v == 0:
+		return 1
+	else:
+		return 0
+
 def satisfies( vec, clauses ):
+	res = [] # values from each clause
+	ins = [ 0 for i in xrange( len( vec ) )] # number of clauses insatisfied ( by variable )
+	sat = 1
+
 	for c in clauses:
+		s = 0 # value of clause
 		for i in xrange( len( c ) ):
+			ab = abs( int( c[i] ) )
+			if int( c[i] ) < 0:
+				value = invValue( vec[ab] )
+			else:
+				value = vec[ab]
+
+			s += value
+			#print c[i], int( c[i] ) > 0, ab, value
+		#print c, s
+		if s == 0:
+			for i in xrange( len( c ) ):
+				ab = abs( int( c[i] ) )
+				ins[ab] += 1
+
+			sat = 0
+
+		res.append( s )
+
+	#print res
+	#print ins
+	return ( sat == 1, ins )
 
 main()
